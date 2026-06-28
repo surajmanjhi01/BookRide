7ad69f181e7c95df49b5a85f93e9e9306882290f
Suraj Manjhi <surajmanjhi303@gmail.com>
2026-06-28 18:18:23 +0530

    updated user routes

--- Backend/routes/user.routes.js
+++ Backend/routes/user.routes.js
@@ -6,6 +6,6 @@ const authMiddleware = require('../middlewares/auth.middleware');
 
 router.post('/register', userController.registerUser);
 router.post('/login', userController.loginUser);
-router.get('/profile', authMiddleware.authUser, userController.getuserProfile);
-
+router.get('/profile', authMiddleware.authUser, userController.getUserProfile);
+router.post('/logout', authMiddleware.authUser, userController.logoutUser);
 module.exports = router;
