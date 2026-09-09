b7ea2c894ee6a07077bb385d26f6667b36da56d5
Suraj Manjhi <surajmanjhi303@gmail.com>
2026-09-09 23:12:01 +0530

    changed frontend env

--- Backend/app.js
+++ Backend/app.js
@@ -22,7 +22,7 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: process.env.FRONTEND_URL || "http://localhost:5173",
     credentials: true,
   })
 );
